2cb429335e9f780c02a7e60f623a3348a294e351
Artur Avtukhov <archy6754267@gmail.com>
2023-03-15 08:12:05 +0100

    Page_Profile_Planshet

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,87 @@
 import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
-import Header from "../header/Header";
+import SearchForm from "../SearchForm/SearchForm";
 
-function Login(props) {
+function Login({
+  onEditProfile,
+}) {
+
+  function onSubmit() {
+    console.log('ssssss')
+  }
 
   return (
-    <main>
+    <>
       <section className="login">
+        <div className="login__wrapper">
 
-        <Header
-        /> 
-      
+       <div className="login_title">
+           <div className="login__logo"></div>
+           <h1 className="login__welcome">Рады видеть!</h1>
+        </div>
+
+        <form
+              onSubmit={onSubmit}
+              className="login_form"
+              method="post"
+              name="login"
+            >
+
+              <p className="login__title">E-mail</p>
+
+              <div className="login__box">
+                <input
+                  id="login__email"
+                  type="text"
+                  //value="pochta@yandex.ru"
+                  // onChange={props.handleOnChange}
+                  name="emailReg"
+                  placeholder=""
+                  className="register__email"
+                  minLength="2"
+                  maxLength="40"
+                  required
+                ></input>
+                <div className="login__line" />
+              </div>
+
+              <p className="login__title">Пароль</p>
+
+              <div className="login__box">
+                <input
+                  id="login__password"
+                  type="password"
+                  //value= "pochta@yandex.ru"
+                  onChange={onSubmit}
+                  name="passwordReg"
+                  placeholder=""
+                  className="register__password"
+                  minLength="2"
+                  maxLength="200"
+                  required
+                ></input>
+                <div className="login__line" />
+              </div>
+              
+              <button type="submit" className="login__register">
+                <p className="login__label">
+                  Войти
+                </p>
+              </button>
+
+              <div className="login__framer">
+                 Еще не зарегистрированы? 
+                <NavLink to="/sign-in" className="login__redirect">
+                  Регистрация
+                </NavLink>
+              </div>
+            </form>
+
+       </div>
       </section>
-    </main>
+    </>
   );
 }
 
